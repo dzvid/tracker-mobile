2c08643f3efcd6618c1417299a3ccbfbf7b26ee5
David <oliveiradavid.dev@gmail.com>
2020-05-17 00:29:14 -0400

    chore: Get permission to track user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Location from 'expo-location';
 
 import Map from '../components/Map';
-import Spacer from '../components/Spacer';
 
 function TrackCreateScreen() {
+  const [err, setErr] = useState(false);
+
+  const startWatching = async () => {
+    // To reset permissions:  adb shell pm reset-permissions
+    const { status } = await Location.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      setErr(true);
+    } else {
+      setErr(false);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text h2>Create a Track</Text>
-
       <Map />
+
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 }
