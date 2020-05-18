7b8cbc4db22bbe923e151e29a3f3b10fee832239
David <oliveiradavid.dev@gmail.com>
2020-05-17 21:26:08 -0400

    chore: Faking user location
    For testing/development purposes it is necessary
    to mock the user location.

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = (increment) => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees,
+      latitude: -3.108519 + increment * tenMetersWithDegrees,
+      longitude: -60.059594 + increment * tenMetersWithDegrees,
     },
   };
 };
